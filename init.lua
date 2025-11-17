f97fa69d8e89e9aeacbc5ac4617bacdc3188537a
Ali moumen <moumenisbest@gmail.com>
2025-11-17 13:03:10 +0100

    updated to neovim 0.11+ syntax and added gopls

--- init.lua
+++ init.lua
@@ -1,42 +1,60 @@
-require("plugins.impatient")
+pcall(require, "impatient")
+vim.env.PATH = vim.env.PATH .. ":" .. os.getenv("HOME") .. "/go/bin"
 vim.opt.number = true
 vim.g.mapleader = " "
 vim.env.PATH = vim.env.PATH .. ':' .. os.getenv("HOME") .. '/.local/bin'
+
 require("config.lazy")
 require("config.keymaps")
-local cmp = require('cmp')
+
+local cmp = require("cmp")
 cmp.setup({
     snippet = {
         expand = function(args)
-        require('luasnip').lsp_expand(args.body)  
-         end,
+            require("luasnip").lsp_expand(args.body)
+        end,
     },
     mapping = {
-        ['<Up>'] = cmp.mapping.select_prev_item(),
-        ['<Down>'] = cmp.mapping.select_next_item(),
-        ['<CR>'] = cmp.mapping.confirm({ select = true }),
-        ['<C-space>'] = cmp.mapping.complete(),
+        ["<Up>"] = cmp.mapping.select_prev_item(),
+        ["<Down>"] = cmp.mapping.select_next_item(),
+        ["<CR>"] = cmp.mapping.confirm({ select = true }),
+        ["<C-Space>"] = cmp.mapping.complete(),
     },
     sources = {
-        { name = 'nvim_lsp' },
-        { name = 'buffer' },
-        { name = 'path' },
-        { name = 'luasnip' },
+        { name = "nvim_lsp" },
+        { name = "buffer" },
+        { name = "path" },
+        { name = "luasnip" },
     },
 })
-local lspconfig = require("lspconfig")
 
-lspconfig.clangd.setup{
-	cmd = { "clangd" }
-}
-require("lualine").setup{
-	options = { theme = 'ayu_dark' }
-}
-lspconfig.pyright = nil 
 require("mason").setup()
-require("mason-lspconfig").setup{
-	ensure_installed = { "pylsp" },
-	automatic_installation = false,
+require("mason-lspconfig").setup({
+    ensure_installed = { "pylsp", "gopls" },
+    automatic_installation = false,
+})
 
-}
-lspconfig.pylsp.setup{}
+
+vim.lsp.config("clangd", {
+    cmd = { "clangd" },
+})
+
+vim.lsp.config("pylsp", {})
+vim.lsp.config("gopls", {
+    cmd = { "gopls" },
+    settings = {
+        gopls = {
+            analyses = {
+                unusedparams = true,
+            },
+            staticcheck = true,
+        },
+    },
+})
+
+vim.lsp.enable({ "clangd", "pylsp", "gopls" })
+
+require("lualine").setup({
+    options = { theme = "ayu_dark" },
+})
+
